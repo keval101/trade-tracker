078025c2146713c42f16ec85e859f2ff2b2ed000
Keval Vadhiya <kevalvadhiya101@gmail.com>
2024-02-08 12:49:27 +0530

    Setup: Flowbite - UI Library with tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
