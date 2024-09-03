a30bea21ef239063cca58b55f63a70beb615cdc5
Keval Vadhiya <kevalvadhiya101@gmail.com>
2024-09-03 16:04:16 +0530

    Added Target Calculator

--- src/app/components/calculate-stoploss/calculate-stoploss.component.ts
+++ src/app/components/calculate-stoploss/calculate-stoploss.component.ts
@@ -9,8 +9,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CalculateStoplossComponent {
 
   public stoplossForm: FormGroup;
+  public targetForm: FormGroup;
   public customStoplossLimit: boolean;
+  public customTargetLimit: boolean;
   public stopLossPrice: number;
+  public targetPrice: number;
   constructor(private fb: FormBuilder) {
     
   }
@@ -21,21 +24,37 @@ export class CalculateStoplossComponent {
       stoploss: [0, Validators.required],
       quantity: [0, Validators.required],
     })
+
+    this.targetForm = this.fb.group({
+      amount: [0, Validators.required],
+      target: [0, Validators.required],
+      quantity: [0, Validators.required],
+    })
   }
 
   setStoplossLimit(limit) {
     this.stoplossForm.get('stoploss').setValue(limit)
-    console.log(this.stoplossForm.value)
     this.customStoplossLimit = false;
   }
 
+  setTargetLimit(limit) {
+    this.targetForm.get('target').setValue(limit)
+    this.customTargetLimit = false;
+  }
+
   calculateStopLoss() {
-    console.log(this.stoplossForm.value)
     const data = this.stoplossForm.value
     const stopLossDecimal = (data.stoploss / 100);
     const stopLossPrice = data.amount * (1 - stopLossDecimal);
     this.stopLossPrice = stopLossPrice;
     return stopLossPrice;
-
   }
+
+  calculateTargetPrice() {
+    const data = this.targetForm.value;
+    const targetDecimal = (data.target / 100);
+    const targetPrice = data.amount * (1 + targetDecimal);
+    this.targetPrice = targetPrice;
+    return targetPrice;
+}
 }
